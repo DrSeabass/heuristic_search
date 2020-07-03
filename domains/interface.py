dbd8c73f66a89ff6b44e0b529b5553d5451f9b5a
Seabass <jordan.thayer@gmail.com>
2020-07-03 12:10:09 -0400

    Defines a search interface

---
+++ domains/interface.py
@@ -0,0 +1,30 @@
+def SearchInterface():
+
+    def __init__(self, problem):
+        pass
+
+    def goal_p(self, state):
+        raise NotImplementedError()
+
+    def get_actions(self, state):
+        raise NotImplementedError()
+
+    def expand(self,state):
+        raise NotImplementedError()
+
+    def initial_state(self):
+        raise NotImplementedError()
+
+    def apply_action(self, state, action):
+        raise NotImplementedError()
+
+    def undo_action(self, state, action):
+        raise NotImplementedError()
+
+    def vailadte_solution(self, action_list):
+        raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    si = SearchInterface()
+    print("Hello World!")
